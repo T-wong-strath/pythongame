84b3a2de1796c7b58f36cdafaf5141155b4407ac
Tim <86616260+T-wong-strath@users.noreply.github.com>
2021-12-09 00:45:34 +0000

    Update base_classes.py

--- base_classes.py
+++ base_classes.py
@@ -23,13 +23,13 @@ messages_dict = {
 def to_user(message,message_type="narrator"):
     color = messages_dict[message_type]
     for c in message:
-        print(colored(c, color), end='',flush=True)
+        print(colored(c, color), end='')
         time.sleep(0.05)
-    print(' ',flush=True)
+    print(' ')
         
     
 class Character(object):
-    def __init__(self, name, health=50, attack=5, defence=5, inventory=[], inventory_space=3, playable=0, friendly=0):
+    def __init__(self, name, health=50, attack=5, defence=5, inventory=[], inventory_space=3, playable=0, friendly=0, equipped=None):
         self.name = name
         self.health = health
         self.attack = attack
@@ -38,6 +38,28 @@ class Character(object):
         self.inventory_space = inventory_space
         self.playable = playable
         self.friendly = friendly
+        self.equipped = equipped
+        
+    def equip_item(self, item):
+        if self.equipped is None:
+            self.equipped = item
+            to_user(item.name + ' has been equipped.')
+        else:
+            to_user(self.equipped.name + ' is currently equipped.')
+            to_user('Do you want to replace it with ' + item.name + '? [Y/N]')
+            
+            while True:
+                equip = input()
+                if equip == 'Y':
+                    self.equipped == item
+                    to_user(item.name + ' has been equipped.')
+                    break
+                elif equip == 'N':
+                    to_user(item.name + ' has not been equipped.')
+                    break
+                else:
+                    to_user('Speak up traveller, please say Y or N')
+
         
     def give_item(self, item):
         if isinstance(item,Item):
@@ -56,12 +78,13 @@ class Character(object):
     #         self.inventory.
         
 class Item(object):
-    def __init__(self, name, item_class):
+    def __init__(self, name, item_class, attackRange=[0,2]):
         self.name = name
         self.item_class = item_class
+        self.attackRange = attackRange
         
-def begin_combat_encounter(characters):
-    turn_order = random.shuffle(characters)
+def begin_combat_encounter(turn_order):
+    random.shuffle(turn_order)
     names_list = []
     friend_list = []
     enemy_list = []
@@ -73,26 +96,49 @@ def begin_combat_encounter(characters):
         else:
             enemy_list.append(player.name)
         
-    # Turns
+    
     for player in turn_order:
+        
+        # Choose target
         if player.playable:
             to_user('Who do you target?')
             to_user(enemy_list)
             ##### Take user command ####
             playerTarget = input()
-            target = enemy_list.index(playerTarget)
+            target_idx = names_list.index(playerTarget)
         elif player.friendly:
-            target = enemy_list.index(random.choice(enemy_list))
+            to_user(player.name + ' is taking a turn')
+            target_idx = names_list.index(random.choice(enemy_list))
         else:
-            target = friend_list.index(random.choice(friend_list))
+            to_user(player.name + ' is taking a turn')
+            target_idx = names_list.index(random.choice(friend_list))
             
-    to_user(target)       
-    # Choose target, 
-    # Roll damage
-    # Check and Add AI damage boosts
-    # Check target defence boosts
-    # Deduct target damage
-    
+        target = turn_order[target_idx]
+        to_user(player.name + ' targets ' + target.name)
+        
+        # Roll damage
+        if player.equipped is not None:
+            attkRange = player.equipped.attackRange
+            attack = player.attack + random.randint(attkRange[0],attkRange[1])
+        else:
+            attack = player.attack
+        ### Check attack augments ###
+        
+        # Check target defence
+        if target.defence >= attack:
+            damage = 0
+        else:
+            damage = attack - target.defence
+        ### Check defence boosts ###
+        to_user(player.name + ' deals ' + str(damage) + ' damage!')
+       
+        # Deduct target health 
+        target.health -= damage
+        print(target.name + '\'s new health is: ' + str(target.health))
+
+    # CHECK HEALTH STATUS 
+    # REMOVE DEAD FROM LIST
+    # IF ENEMYLIST NOT EMPTY, TAKE NEW TURN
     return
 
 
@@ -106,20 +152,24 @@ user_name = input()
 
 # Reply and ask for weapon name
 to_user('Hello '+user_name+'!')
-player = Character(user_name, friendly=1)
+player = Character(user_name, friendly=1, playable =1)
 to_user('I have a sword here for you, what will you name it?')
 weapon_name = input()
 
 # Add the weapon to the player inventory
-player_weapon = Item(weapon_name, 'weapon')
+player_weapon = Item(weapon_name, 'weapon', attackRange=[5,10])
 player.give_item(player_weapon)
+
+# Equip item
+to_user('Let\'s equip ' + player_weapon.name)
+player.equip_item(player_weapon)
 
 # Create an evil boi
 enemy = Character('Goblin')
 to_user('A wild '+str(enemy.name)+' has appeared!')
 
-# Test comment
-
-
+# Begin combat
+characters = [player,enemy]
+begin_combat_encounter(characters)
 
 
